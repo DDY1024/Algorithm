afe27fb093eae859a9768efa39ebfaa1806132f5
wangxiyang <wangxiyang@bytedance.com>
2021-04-15 10:00:37 +0800

    feat: add
    Change-Id: I685a70009b414635fa2a0b10564c25ca87cfa710

---
+++ Library/int_heap.go
@@ -0,0 +1,54 @@
+package Library
+
+// 1. 小顶堆
+type MinHeap []int
+
+func (h MinHeap) Len() int           { return len(h) }
+func (h MinHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h MinHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *MinHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *MinHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+// heap 标准库内部做了调整 --> 因此我们每次取最值元素只需要关注 "0" 索引位置处的元素即可。
+func (h *MinHeap) Top() interface{} {
+	if h.Len() == 0 {
+		return nil
+	}
+	return (*h)[0]
+}
+
+// 2. 大顶堆
+type MaxHeap []int
+
+func (h MaxHeap) Len() int           { return len(h) }
+func (h MaxHeap) Less(i, j int) bool { return h[i] > h[j] }
+func (h MaxHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *MaxHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *MaxHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+func (h *MaxHeap) Top() interface{} {
+	if h.Len() == 0 {
+		return nil
+	}
+	return (*h)[0]
+}
---
+++ Library/priority_queue.go
@@ -0,0 +1,79 @@
+package Library
+
+// 结构体自定义: 明确优先级比较规则
+type Item struct {
+	val int
+	idx int
+}
+
+// 1. 大顶堆
+type MaxPQ []*Item
+
+func (pq MaxPQ) Len() int { return len(pq) }
+
+func (pq MaxPQ) Less(i, j int) bool {
+	if pq[i].val == pq[j].val {
+		return pq[i].idx > pq[j].idx
+	}
+	return pq[i].val > pq[j].val
+}
+
+func (pq MaxPQ) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+}
+
+func (pq *MaxPQ) Push(x interface{}) {
+	item := x.(*Item)
+	*pq = append(*pq, item)
+}
+
+func (pq *MaxPQ) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	*pq = old[0 : n-1]
+	return item
+}
+
+func (pq *MaxPQ) Top() interface{} {
+	if pq.Len() == 0 {
+		return nil
+	}
+	return (*pq)[0]
+}
+
+// 2. 小顶堆
+type MinPQ []*Item
+
+func (pq MinPQ) Len() int { return len(pq) }
+
+func (pq MinPQ) Less(i, j int) bool {
+	if pq[i].val == pq[j].val {
+		return pq[i].idx < pq[j].idx
+	}
+	return pq[i].val < pq[j].val
+}
+
+func (pq MinPQ) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+}
+
+func (pq *MinPQ) Push(x interface{}) {
+	item := x.(*Item)
+	*pq = append(*pq, item)
+}
+
+func (pq *MinPQ) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	*pq = old[0 : n-1]
+	return item
+}
+
+func (pq *MinPQ) Top() interface{} {
+	if pq.Len() == 0 {
+		return nil
+	}
+	return (*pq)[0]
+}
