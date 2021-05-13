efe6bedc7e4a61bf9f72d5d003126d5923c6bc3b
wangxiyang <wangxiyang@bytedance.com>
2021-05-13 13:43:29 +0800

    feat: add
    Change-Id: Id3bf4898bb074d9287d9e9c1bfc57fe3649c7948

--- test.go
+++ test.go
@@ -1,95 +1,32 @@
 package main
 
-import (
-	"container/heap"
-	"sort"
-)
-
-type Item struct {
-	l, r int
-}
-
-type MinPQ []*Item
-
-func (pq MinPQ) Len() int { return len(pq) }
-
-func (pq MinPQ) Less(i, j int) bool {
-	return (pq[i].r - pq[i].l) <= (pq[j].r - pq[j].l)
-}
-
-func (pq MinPQ) Swap(i, j int) {
-	pq[i], pq[j] = pq[j], pq[i]
-}
-
-func (pq *MinPQ) Push(x interface{}) {
-	item := x.(*Item)
-	*pq = append(*pq, item)
-}
-
-func (pq *MinPQ) Pop() interface{} {
-	old := *pq
-	n := len(old)
-	item := old[n-1]
-	*pq = old[0 : n-1]
-	return item
-}
-
-func (pq *MinPQ) Top() interface{} {
-	if pq.Len() == 0 {
-		return nil
+func minInt(a, b int) int {
+	if a < b {
+		return a
 	}
-	return (*pq)[0]
+	return b
 }
 
-type Query struct {
-	pos int
-	idx int
-}
-
-func minInterval(intervals [][]int, queries []int) []int {
-	n, m := len(intervals), len(queries)
-	qs := make([]*Query, 0, m)
-	for i := 0; i < m; i++ {
-		qs = append(qs, &Query{
-			pos: queries[i],
-			idx: i,
-		})
+func numWays(steps int, arrLen int) int {
+	n, m, mod := steps, minInt(steps+1, arrLen), int(1e9+7)
+	dp := make([][]int, n+1)
+	for i := 0; i <= n; i++ {
+		dp[i] = make([]int, m)
 	}
-	sort.Slice(qs, func(i, j int) bool {
-		return qs[i].pos < qs[j].pos
-	})
-
-	intervalList := make([]*Item, 0, n)
-	for i := 0; i < n; i++ {
-		intervalList = append(intervalList, &Item{
-			l: intervals[i][0],
-			r: intervals[i][1],
-		})
-	}
-	sort.Slice(intervalList, func(i, j int) bool {
-		if intervalList[i].l == intervalList[j].l {
-			return intervalList[i].r < intervalList[j].r
-		}
-		return intervalList[i].l < intervalList[j].l
-	})
-
-	hp, idx, ans := make(MinPQ, 0, n), 0, make([]int, m)
-	for i := 0; i < m; i++ {
-		for idx < n && intervalList[idx].l <= qs[i].pos {
-			heap.Push(&hp, intervalList[idx])
-			idx++
-		}
-		for hp.Len() > 0 && hp.Top().(*Item).r < qs[i].pos {
-			heap.Pop(&hp)
-		}
-		if hp.Len() > 0 {
-			item := hp.Top().(*Item)
-			ans[qs[i].idx] = item.r - item.l + 1
-		} else {
-			ans[qs[i].idx] = -1
+	dp[0][0] = 1
+	for i := 1; i <= n; i++ {
+		for j := 0; j < minInt(i+1, m); j++ {
+			dp[i][j] = dp[i-1][j]
+			if j-1 >= 0 {
+				dp[i][j] += dp[i-1][j-1]
+			}
+			if j+1 < m {
+				dp[i][j] += dp[i-1][j+1]
+			}
+			dp[i][j] %= mod
 		}
 	}
-	return ans
+	return dp[n][0]
 }
 
 func main() {
