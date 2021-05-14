99d6656f9cfde9737a111092f0b4794369ee7043
wangxiyang <wangxiyang@bytedance.com>
2021-05-14 10:13:25 +0800

    feat: add
    Change-Id: Ic43d4ba0d5bfd8ffb651e86dc10005e7ea2e61cb

--- test.go
+++ test.go
@@ -1,34 +1,33 @@
 package main
 
-func minInt(a, b int) int {
-	if a < b {
-		return a
+func firstMissingPositive(nums []int) int {
+	n, idx := len(nums), 0
+	for idx < n {
+		if nums[idx] == idx+1 {
+			idx++
+			continue
+		}
+		if nums[idx] <= 0 || nums[idx] > n {
+			idx++
+			continue
+		}
+		if nums[idx] == nums[nums[idx]-1] {
+			idx++
+			continue
+		}
+		nums[idx], nums[nums[idx]-1] = nums[nums[idx]-1], nums[idx]
 	}
-	return b
-}
 
-func numWays(steps int, arrLen int) int {
-	n, m, mod := steps, minInt(steps+1, arrLen), int(1e9+7)
-	dp := make([][]int, n+1)
-	for i := 0; i <= n; i++ {
-		dp[i] = make([]int, m)
-	}
-	dp[0][0] = 1
-	for i := 1; i <= n; i++ {
-		for j := 0; j < minInt(i+1, m); j++ {
-			dp[i][j] = dp[i-1][j]
-			if j-1 >= 0 {
-				dp[i][j] += dp[i-1][j-1]
-			}
-			if j+1 < m {
-				dp[i][j] += dp[i-1][j+1]
-			}
-			dp[i][j] %= mod
+	ans := n + 1
+	for i := 0; i < n; i++ {
+		if nums[i] != i+1 {
+			ans = i + 1
+			break
 		}
 	}
-	return dp[n][0]
+	return ans
 }
 
 func main() {
-
+	firstMissingPositive([]int{1, 1})
 }
