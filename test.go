452fefafcefc74968ae4f21cfec0f0ff1c46c0d4
wangxiyang <wangxiyang@bytedance.com>
2021-05-16 20:11:35 +0800

    feat: add

--- test.go
+++ test.go
@@ -1,33 +1,27 @@
 package main
 
-func firstMissingPositive(nums []int) int {
-	n, idx := len(nums), 0
-	for idx < n {
-		if nums[idx] == idx+1 {
-			idx++
-			continue
-		}
-		if nums[idx] <= 0 || nums[idx] > n {
-			idx++
-			continue
-		}
-		if nums[idx] == nums[nums[idx]-1] {
-			idx++
-			continue
-		}
-		nums[idx], nums[nums[idx]-1] = nums[nums[idx]-1], nums[idx]
+func minInt(a, b int) int {
+	if a < b {
+		return a
 	}
-
-	ans := n + 1
-	for i := 0; i < n; i++ {
-		if nums[i] != i+1 {
-			ans = i + 1
-			break
-		}
-	}
-	return ans
+	return b
 }
 
-func main() {
-	firstMissingPositive([]int{1, 1})
+func rearrangeSticks(n int, k int) int {
+	dp := make([][]int, n+1)
+	for i := 0; i <= n; i++ {
+		dp[i] = make([]int, k+1)
+	}
+	mod := int(1e9 + 7)
+	dp[0][0] = 1
+	for i := 1; i <= n; i++ {
+		for j := 0; j <= minInt(i, k); j++ {
+			dp[i][j] = dp[i-1][j] * (n - i) % mod
+			if j-1 >= 0 {
+				dp[i][j] += dp[i-1][j-1]
+				dp[i][j] %= mod
+			}
+		}
+	}
+	return dp[n][k]
 }
