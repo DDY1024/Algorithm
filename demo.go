18a5b1ab7bebdd9580e1babbc10b7383b82e8642
wangxiyang <wangxiyang@bytedance.com>
2021-06-19 14:20:37 +0800

    feat: add
    Change-Id: I24b34968b6882b8f9da6fe2109f1938d1ff7bae7

--- demo.go
+++ demo.go
@@ -1,107 +1,89 @@
 package main
 
-import "fmt"
-
-type (
-	State    = int
-	CharType = int
+import (
+	"fmt"
+	"math"
+	"math/bits"
+	"strconv"
 )
 
-const (
-	StateInitial             State = iota // 0: 初始状态
-	StateSign                             // 1: 符号位
-	StateInteger                          // 2: 整数部分
-	StatePointWithInteger                 // 3: 左侧有整数的小数点
-	StatePointWithoutInteger              // 4: 左侧无整数的小数点
-	StateFraction                         // 5: 小数部分
-	StateExp                              // 6: 指数 e
-	StateExpSign                          // 7: 指数符号位 e+1, e-1
-	StateExpInteger                       // 8: 指数整数部分
-	StateEnd                              // 9: 结束状态
-)
-
-const (
-	CharInteger CharType = iota // 0: 整数
-	CharExp                     // 1: 指数 e 或 E
-	CharPoint                   // 2: 小数点
-	CharSign                    // 3: 符号位 -/+
-	CharIllegal                 // 4: 非法字符
-)
-
-// 定义 FSM 状态转换关系即每种状态下遇到某类字符时的状态转移
-// 其实本质上状态转换已经对整数的表示形式进行了相对应的限制了
-var transation = map[State]map[CharType]State{
-	StateInitial: {
-		CharInteger: StateInteger,
-		CharPoint:   StatePointWithoutInteger,
-		CharSign:    StateSign,
-	},
-	StateSign: {
-		CharInteger: StateInteger,
-		CharPoint:   StatePointWithoutInteger,
-	},
-	StateInteger: {
-		CharInteger: StateInteger,
-		CharExp:     StateExp,
-		CharPoint:   StatePointWithInteger,
-	},
-	StatePointWithInteger: {
-		CharInteger: StateFraction, // 进入小数部分了
-		CharExp:     StateExp,
-	},
-	StatePointWithoutInteger: {
-		CharInteger: StateFraction,
-	},
-	StateFraction: {
-		CharInteger: StateFraction, // 小数部分
-		CharExp:     StateExp,
-	},
-	StateExp: {
-		CharInteger: StateExpInteger,
-		CharSign:    StateExpSign,
-	},
-	StateExpSign: {
-		CharInteger: StateExpInteger,
-	},
-	StateExpInteger: {
-		CharInteger: StateExpInteger,
-	},
-}
-
-func toCharType(ch byte) CharType {
-	switch ch {
-	case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
-		return CharInteger
-	case 'e', 'E':
-		return CharExp
-	case '.':
-		return CharPoint
-	case '+', '-':
-		return CharSign
-	default:
-		return CharIllegal
-	}
-}
-
-func isLegalEndState(state State) bool {
-	return state == StateInteger || state == StatePointWithInteger || state == StateFraction || state == StateExpInteger
-}
-
-func isNumber(s string) bool {
-	state := StateInitial
-	for i := 0; i < len(s); i++ {
-		typ := toCharType(s[i])
-		if typ == CharIllegal {
-			return false
+func power(a, b int) int {
+	res := 1
+	for b > 0 {
+		if b&1 > 0 {
+			res = res * a
 		}
-		if _, ok := transation[state][typ]; !ok {
-			return false
-		}
-		state = transation[state][typ]
+		b >>= 1
+		a = a * a
 	}
-	return isLegalEndState(state)
+	return res
 }
+
+func calc(a, b int) int {
+	ans := 0
+	for i := 0; i <= b; i++ {
+		ans += power(a, i)
+	}
+	return ans
+}
+
+func smallestGoodBase(n string) string {
+	ni, _ := strconv.ParseInt(n, 10, 64)
+	maxL := bits.Len(uint(ni)) - 1
+	for i := maxL; i > 1; i-- {
+		k := int(math.Pow(float64(ni), 1.0/float64(i)))
+		if calc(k, i) == int(ni) {
+			return strconv.FormatInt(int64(k), 10)
+		}
+
+	}
+	return strconv.FormatInt(ni-1, 10)
+}
+
+// k^(i+1) - 1 / (k-1)  ni
+// 2^n-1
+// 2^n - 1
+//
+// (K+1)^m
+//
+// K^0 + K^1 + ... + K^x = (K^(x+1)-1)/(K-1)
+// 二项式公式
+// (K+1)^m = C(m, 0) * K^0 + ... + C(m, m) * K^m
+//
+// 二项式公式决定了其单调递增特性
+
+// k^
+// (k+1)^m
 
 func main() {
-	fmt.Println("Hello, World!")
+	// fmt.Println(smallestGoodBase("13"))
+	// fmt.Println(power(686286299, 3))
+	fmt.Println(smallestGoodBase("470988884881403701"))
 }
+
+// 10^9
+//
+//
+//
+//
+// k^1
+// k^1
+// a^0 + a^1 + a^2 ... + a^k = b
+// a = c^0 + c^1 + ... + c^k
+// c^0 + c^2 + ... + c^k
+// 10^9
+// 10^9 + 1
+// 10^18 + 1 1,1,10^9^2
+// 1000,0000, 10^14
+// 1 + 10^7 + 10^14
+// 1 + 10^8 + 10^16
+// 1 + 10^9 + 10^18
+//
+// 2^64
+//
+//
+// 10^18 --> 10^5
+//
+//
+//
+//
