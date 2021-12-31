fd06f6275dc400f88e00b9e16098acc04ac9f0d0
wangxiyang <wangxiyang@bytedance.com>
2021-12-31 19:24:05 +0800

    feat: add
    Change-Id: I684b5f1ca919d0930ee3441c98db5cbb800792a9

--- Problem/ds/字典树/lt_472(字典树、搜索).go
+++ Problem/ds/字典树/lt_472(字典树、搜索).go
@@ -50,6 +50,8 @@ func findAllConcatenatedWordsInADict(words []string) []string {
 	var check func(nd *Node, word string) bool
 	// 学会 trie 上如何进行 dfs 搜索
 	check = func(nd *Node, word string) bool {
+		// 由于数组 \textit{words}words 中没有重复的单词，因此在判断一个单词是不是连接词时，
+		// 该单词一定没有加入字典树，由此可以确保判断连接词的条件成立。
 		if len(word) == 0 { // 小拼大肯定是大于 1 个的
 			return true
 		}
@@ -59,7 +61,7 @@ func findAllConcatenatedWordsInADict(words []string) []string {
 				return false
 			}
 			nd = nd.child[getChildIndex(byte(ch))]
-			if nd.flag && check(root, word[i+1:]) {
+			if nd.flag && check(root, word[i+1:]) { // 此处状态转移可以记忆化
 				return true
 			}
 		}
@@ -71,6 +73,8 @@ func findAllConcatenatedWordsInADict(words []string) []string {
 		if len(words[i]) == 0 {
 			continue
 		}
+		// 由于一个连接词由多个更短的非空单词组成，如果存在一个较长的连接词的组成部分之一是一个较短的连接词，
+		// 则一定可以将这个较短的连接词换成多个更短的非空单词，因此不需要将连接词加入字典树。
 		if check(root, words[i]) { // 优化: 对于可以被其它更短字符串拼接而成的字符串，不需要加入字典树，只会增加 dfs 搜索的复杂度
 			ret = append(ret, words[i])
 		} else {
